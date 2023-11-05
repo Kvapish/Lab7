2f6597565df2bf7e9ea1744fa7476828ff065fda
Kvapish Alexander <kvapis@gmail.com>
2023-11-05 09:50:23 +0200

    Update

---
+++ DataHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+/// </summary>
+/// <summary>
+/// Класс DataHandler создан для управления операциями сохранения и восстановления данных.
+/// </summary>
+public class DataHandler
+{
+    public List<int> LoadUserGuesses(string fileName)
+    {
+        List<int> userGuesses = new List<int>();
+        if (File.Exists(fileName))
+        {
+            string json = File.ReadAllText(fileName);
+            userGuesses = JsonConvert.DeserializeObject<List<int>>(json);
+        }
+        return userGuesses;
+    }
+
+    public void SaveUserGuesses(string fileName, List<int> userGuesses)
+    {
+        string json = JsonConvert.SerializeObject(userGuesses);
+        File.WriteAllText(fileName, json);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
+
 /// <summary>
 /// Вариант 8
-/// Создал список для сохранения попыток пользователя, в этом списке будут храниться введенные пользователем числа,сгенерировал случайное число, которое пользователь должен угадать,
-/// пользователь будет вводить числа и они будут добавляться в список userGuesses. List был выбран, так как он обеспечивает удобное добавление и хранение элементов в последовательном порядке. 
-/// </summary>
+/// Игра "Угадай число" (List): Создайте игру, в которой программа случайным образом выбирает число, а пользователь должен угадать его. Используйте список для сохранения попыток пользователя.
+/// Соблюдая  Microsoft C# code conventions и средства языка C# для задания из предыдущей работы реализовать сохранение и восстановление данных данных массивов объектов:
+/// используя сохранение/восстановление в бинарном файле;
+/// используя сериализацию/десериализацию в JSON или XML (на выбор);дополнительно оценивается реализация вышеуказанных операций при помощи отдельного класса.
+/// Код использует библиотеку Newtonsoft.Json для сериализации и десериализации данных в формат JSON.
+///Данные о попытках пользователя сохраняются в файл userGuesses.json и восстанавливаются из него при запуске программы.
+/// 
 class Program
 {
     static void Main()
     {
-        List<int> userGuesses = new List<int>(); 
+        DataHandler dataHandler = new DataHandler();
+        string fileName = "userGuesses.json";
+        List<int> userGuesses = dataHandler.LoadUserGuesses(fileName);
+
         Random random = new Random();
         int secretNumber = random.Next(1, 101);
         int attempts = 0;
+
         while (true)
         {
             Console.Write("Угадайте число (от 1 до 100): ");
@@ -22,11 +31,13 @@ class Program
             {
                 userGuesses.Add(userGuess);
                 attempts++;
+
                 if (userGuess == secretNumber)
                 {
                     Console.WriteLine($"Поздравляем! Вы угадали число {secretNumber} с {attempts} попыток.");
+                    dataHandler.SaveUserGuesses(fileName, userGuesses);
                     Console.WriteLine("Для завершения программы, нажмите любую клавишу...");
-                    Console.ReadKey(); 
+                    Console.ReadKey();
                     break;
                 }
                 else if (userGuess < secretNumber)
@@ -45,3 +56,4 @@ class Program
         }
     }
 }
+
